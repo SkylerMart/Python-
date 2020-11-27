c73a43e77f97abc6577d1f0b18a3572b8ee97072
SkylerMart <71092892+SkylerMart@users.noreply.github.com>
2020-11-27 17:39:18 -0600

    Add files via upload

---
+++ python_list.py
@@ -0,0 +1,19 @@
+import sqlite3
+conn = sqlite3.connect('filelist.db')
+with conn:
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS tbl_fileNames (ID INTEGER PRIMARY KEY AUTOINCREMENT,\
+        col_fname TEXT)")
+    conn.commit()
+conn = sqlite3.connect('filelist.db')
+
+fileList = ('information.dox', 'Hello.txt','myImage.png',\
+            'myMovie.mpg','World.txt','data.pdf','myPhoto/jpg')
+for x in fileList:
+    if x.endswith('.txt'):
+        with conn:
+            cur = conn.cursor()
+            cur.execute("INSERT INTO tbl_fileNames (col_fname) VALUES (?)",(x,))
+            print(x)
+conn.close
+
